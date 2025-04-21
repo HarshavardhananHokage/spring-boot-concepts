dfc0defd5561fd3d3eecbf169776e0fb6a8d9aec
Harshavardhanan <harshavardhan2990@gmail.com>
2025-04-21 19:58:42 +0530

    first commit

---
+++ src/test/java/com/harsh/spring/boot/concepts/SpringBootConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.spring.boot.concepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
